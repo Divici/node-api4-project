9ae1eb4e921b73b4351017c21177657a989573b5
david-aihe <doa9200@gmail.com>
2022-02-10 15:16:45 -0500

    edits

--- api/server.js
+++ api/server.js
@@ -4,10 +4,17 @@ const cors = require('cors')
 const server = express()
 const userRouter = require('../users/user-router')
 
+const path = require("path")
+
 server.use(cors())
 server.use(express.json())
+server.use(express.static(path.join(__dirname, "client/build")))
 
 server.use('/api', userRouter)
+
+server.use("/api/", (_, res)=>{
+    res.json({data:"API is accounted for"}) //api used will be put here
+})
 
 server.use("*", (req, res)=>{
     res.status(404).json({message: 'nothing found'})
